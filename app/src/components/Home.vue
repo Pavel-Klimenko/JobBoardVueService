bb714243f00439c79755f2cd547e5749e5a09322
Pavel Klimenko <p.klimenko@itachsoft.com>
2024-06-10 14:09:09 +0300

    Created homepage blocks

--- app/src/components/Home.vue
+++ app/src/components/Home.vue
@@ -1,14 +1,117 @@
 <template>
-
-
-
   <Header />
   <!--      <Slider />-->
-  <!--      <FindJob />-->
-  <!--      <JobCategories />-->
-  <JobListing />
+        <FindJob />
+
+<!--    <div class="popular_category_area" v-if="this.homePageData.job_categories">-->
+<!--    <div class="container">-->
+<!--      <div class="row">-->
+<!--        <div class="col-lg-12">-->
+<!--          <div class="section_title mb-40">-->
+<!--            <h3>Job Categories</h3>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--      <div class="row">-->
+<!--        <div class="col-lg-4 col-xl-3 col-md-6" v-for="(category, index) in this.homePageData.job_categories">-->
+<!--          <div class="single_catagory">-->
+<!--            <a :href="`/browse-job?CATEGORY_ID=${category.ID}`">-->
+<!--              <h4>{{category.NAME}}</h4>-->
+<!--            </a>-->
+<!--            <p><span>{{category.QUANTITY_OF_VACANCIES}}</span> Available positions</p>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--  </div>-->
+
+
+    <div class="job_listing_area" v-if="this.homePageData.vacancies">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-6">
+            <div class="section_title">
+              <h3>Job Listing</h3>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="brouse_job text-right">
+              <a href="#" class="boxed-btn4">Browse More Job</a>
+            </div>
+          </div>
+        </div>
+        <div class="job_lists">
+          <div class="row">
+            <div class="col-lg-12 col-md-12" v-for="(vacancy, index) in this.homePageData.vacancies">
+              <div class="single_jobs white-bg d-flex justify-content-between">
+                <div class="jobs_left d-flex align-items-center">
+                  <div class="thumb">
+                    <img :src="svgIcon" :alt="svgIcon">
+                  </div>
+                  <div class="jobs_conetent">
+                    <a href="#">
+                      <h4>{{vacancy.NAME}}</h4></a>
+                    <div class="links_locat d-flex align-items-center">
+
+                      <div class="location">
+                        Salary from: <b>{{vacancy.SALARY_FROM}} $</b>
+                      </div>
+
+                      <div class="location">
+                        <p>
+                          <i class="fa fa-map-marker"></i>
+                          {{vacancy.CITY}},
+                          {{vacancy.COUNTRY}}
+                        </p>
+                      </div>
+
+                    </div>
+                  </div>
+                </div>
+                <div class="jobs_right">
+                  <div class="apply_now">
+                    <a href="#" class="boxed-btn3">Look more</a>
+                  </div>
+                  <div class="date">
+                    <p>Created at {{vacancy.created_at}}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+
   <!--      <Candidates />-->
-  <Companies />
+
+    <div class="top_companies_area" v-if="this.homePageData.companies">
+      <div class="container">
+        <div class="row align-items-center mb-40">
+          <div class="col-lg-6 col-md-6">
+            <div class="section_title">
+              <h3>Companies</h3>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 col-xl-3 col-md-6" v-for="(company, index) in this.homePageData.companies">
+            <div class="single_company">
+              <div class="thumb">
+                <img :src="svgIcon" :alt="svgIcon">
+              </div>
+              <a :href="`/browse-job?COMPANY_ID=${company.id}`"><h3>{{company.NAME}}</h3></a>
+              <p><span>{{company.QUANTITY_OF_VACANCIES}}</span> Available position</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
   <!--      <LookingForJob />-->
   <!--      <Reviews />-->
   <Footer />
@@ -20,9 +123,13 @@ import Footer from "./Footer";
 import FindJob from "./homepage/FindJob";
 import Slider from "./homepage/Slider";
 import JobCategories from "./homepage/JobCategories";
-import JobListing from "./homepage/JobListing";
+
+//import JobListing from "./homepage/_JobListing";
+
 import Candidates from "./homepage/Candidates";
-import Companies from "./homepage/Companies";
+
+//import Companies from "./homepage/_Companies";
+
 import LookingForJob from "./homepage/LookingForJob";
 import Reviews from "./homepage/Reviews";
 import axios from 'axios';
@@ -37,34 +144,39 @@ export default {
     Slider,
     FindJob,
     JobCategories,
-    JobListing,
+    //JobListing,
     Candidates,
-    Companies,
+    //Companies,
     LookingForJob,
     Reviews
   },
-  mounted() {
-    this.homePageData = this.getHomePageData();
-
-
-    console.log(GLOBAL_CONSTANTS.APP_JOBSERVICE_URL);
-    console.log(this.homePageData);
-
-  },
-  methods: {
-    getHomePageData: function () {
-      return axios.get(`${GLOBAL_CONSTANTS.APP_JOBSERVICE_URL}/api/homepage/`,{
-            headers: {
-              'Content-Type': 'application/json',
-            }
-          })
-          .then(function (response) {
-            return response;
-          })
-          .catch(function (error) {
-            console.log(error);
-          })
+  data: function(){
+    return {
+      homePageData: []
     }
+  },
+  methods:{
+    getData: function () {
+      axios.get(`${GLOBAL_CONSTANTS.APP_JOBSERVICE_URL}/api/homepage/`, {
+        headers: {'Content-Type': 'application/json'}
+      }).then((response) => {
+        this.homePageData = response.data;
+
+        console.log(this.homePageData);
+      });
+    }
+  },
+  computed: {
+    svgIcon() {
+      let min = 1;
+      let max = 4;
+
+      let number = Math.floor(min + Math.random() * (max - min));
+      return require(`@/assets/img/svg_icon/${number}.svg`);
+    },
+  },
+  mounted(){
+    this.getData();
   }
 }
 </script>
--- app/src/components/homepage/_JobListing.vue
+++ app/src/components/homepage/_JobListing.vue
@@ -10,94 +10,48 @@
         <div class="col-lg-6">
           <div class="brouse_job text-right">
             <a href="#" class="boxed-btn4">Browse More Job</a>
-
           </div>
         </div>
       </div>
       <div class="job_lists">
         <div class="row">
-          <div class="col-lg-12 col-md-12">
+          <div class="col-lg-12 col-md-12" v-for="(vacancy, index) in this.homePageData.vacancies">
             <div class="single_jobs white-bg d-flex justify-content-between">
-
               <div class="jobs_left d-flex align-items-center">
                 <div class="thumb">
                   <img :src="svgIcon2" :alt="svgIcon2">
                 </div>
                 <div class="jobs_conetent">
                   <a href="#">
-
-                    <h4>PHP Developer</h4></a>
+                    <h4>{{vacancy.NAME}}</h4></a>
                   <div class="links_locat d-flex align-items-center">
 
                     <div class="location">
-
-                      Salary from: <b>1500$</b>
+                      Salary from: <b>{{vacancy.SALARY_FROM}} $</b>
                     </div>
 
                     <div class="location">
                       <p>
                         <i class="fa fa-map-marker"></i>
-                        Minsk,
-                        Belarus
+                        {{vacancy.CITY}},
+                        {{vacancy.COUNTRY}}
                       </p>
                     </div>
 
                   </div>
                 </div>
               </div>
-
               <div class="jobs_right">
                 <div class="apply_now">
                   <a href="#" class="boxed-btn3">Look more</a>
-
                 </div>
                 <div class="date">
-                  <p>updated at 2024-02-05 12:15:14</p>
+                  <p>Created at {{vacancy.created_at}}</p>
                 </div>
               </div>
             </div>
           </div>
-          <div class="col-lg-12 col-md-12">
-            <div class="single_jobs white-bg d-flex justify-content-between">
 
-              <div class="jobs_left d-flex align-items-center">
-                <div class="thumb">
-                  <img :src="svgIcon5" :alt="svgIcon5">
-                </div>
-                <div class="jobs_conetent">
-                  <a href="#">
-
-                    <h4>Middle Swift developer</h4></a>
-                  <div class="links_locat d-flex align-items-center">
-
-                    <div class="location">
-
-                      Salary from: <b>2500$</b>
-                    </div>
-
-                    <div class="location">
-                      <p>
-                        <i class="fa fa-map-marker"></i>
-                        Minsk,
-                        Belarus
-                      </p>
-                    </div>
-
-                  </div>
-                </div>
-              </div>
-
-              <div class="jobs_right">
-                <div class="apply_now">
-                  <a href="#" class="boxed-btn3">Look more</a>
-
-                </div>
-                <div class="date">
-                  <p>updated at 2024-01-12 11:30:39</p>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
