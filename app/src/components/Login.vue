329954c779f0ebd8629e58d3fbf4679bd77405c4
Pavel Klimenko <p.klimenko@itachsoft.com>
2024-07-04 11:26:47 +0300

    Developing login form

--- app/src/components/Login.vue
+++ app/src/components/Login.vue
@@ -1,32 +1,36 @@
 <template>
-  <Header />
+  <Header/>
+  <Slider/>
 
-    <div class="login-page">
-      <transition name="fade">
-      </transition>
-      <div class="wallpaper-register"></div>
+  <div class="login-page">
+    <transition name="fade"></transition>
+    <div class="wallpaper-register"></div>
+    <div class="container">
 
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-4 col-md-6 col-sm-8 mx-auto">
-              <h1>Login</h1>
-              <form class="form-group">
-                <input v-model="email" type="email" class="form-control" placeholder="Email" required>
-                <input v-model="password" type="password" class="form-control" placeholder="Password" required>
-                <input type="submit" value="login" class="btn btn-primary" @click.prevent="login">
-              </form>
-            </div>
-          </div>
+      <div class="row">
+        <div v-if="authStatus == 'success'" class="col-lg-4 col-md-6 col-sm-8 mx-auto">
+            <p>User {{email}} successfully authorized</p>
         </div>
 
+        <div v-else-if="authStatus == 'error'" class="col-lg-4 col-md-6 col-sm-8 mx-auto">
+          <h1>Login</h1><br/>
+          <form class="form-group">
+            <input v-model="email" type="email" class="form-control" placeholder="Email" required>
+            <input v-model="password" type="password" class="form-control" placeholder="Password" required>
+            <input type="submit" value="login" class="btn btn-primary" @click.prevent="login">
+          </form>
+        </div>
       </div>
-  <Footer />
+    </div>
+  </div>
+  <Footer/>
 </template>
 
 
 <script>
 import Header from "./Header";
 import Footer from "./Footer";
+import Slider from "./homepage/Slider";
 
 import axios from 'axios';
 
@@ -39,28 +43,47 @@ export default {
   data() {
     return {
       email: null,
-      password: null
+      password: null,
+      authStatus: 'error'
     }
   },
   methods: {
     login() {
-      axios.get(`${GLOBAL_CONSTANTS.APP_JOBSERVICE_URL}/sanctum/csrf-cookie`).then(() => {
-        axios.post(`${GLOBAL_CONSTANTS.APP_JOBSERVICE_URL}/login`, {
-              email: this.email,
-              password: this.password,
-            }).then((response) => {
-              console.log(response.data);
-            })
-            .catch(function (error) {
-              console.error(error);
-            });
-      });
+      console.log(this.email);
+      console.log(this.password);
+
+      axios.post(`${GLOBAL_CONSTANTS.APP_JOBSERVICE_URL}/api/auth/login`, {
+            email: this.email,
+            password: this.password,
+          }, {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }
+      ).then((response) => {
+        console.log(response.data);
+        this.authStatus = response.data.status
+
+        if (response.data.status == 'error') {
+          alert(response.data.message);
+        }
+      })
+          .catch(function (error) {
+            console.error(error);
+          });
     }
   },
   components: {
     Header,
     Footer,
+    Slider,
   },
 }
 </script>
+
+<style>
+.login-page {
+  margin: 5% auto;
+}
+</style>
 
