63d5520120bd55a2bd0076f0d249d5f4494cc803
Pavel Klimenko <p.klimenko@itachsoft.com>
2024-07-31 17:54:16 +0300

    Developed front-end candidates, vacancies list

--- app/src/components/Candidates.vue
+++ app/src/components/Candidates.vue
@@ -1,107 +1,95 @@
 <template>
   <Header />
+  <div class="job_listing_area plus_padding">
+    <div class="container">
+      <div class="row">
 
-  CANDIDATES!
-<!--  <div class="job_listing_area plus_padding">-->
-<!--    <div class="container">-->
-<!--      <div class="row">-->
-<!--        <div class="col-lg-3">-->
-<!--        </div>-->
-<!--        <div class="col-lg-9">-->
-<!--          <div class="recent_joblist_wrap">-->
-<!--            <div class="recent_joblist white-bg ">-->
-<!--              <div class="row align-items-center">-->
-<!--                <div class="col-md-6">-->
-<!--                  <h4>List of vacancies</h4>-->
-<!--                </div>-->
-<!--              </div>-->
-<!--            </div>-->
-<!--          </div>-->
+        <div class="col-lg-9">
+          <div class="recent_joblist_wrap">
+            <div class="recent_joblist white-bg ">
+              <div class="row align-items-center">
+                <div class="col-md-6">
+                  <h4>Candidates</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="job_lists m-0" v-if="this.candidates.data">
+            <div class="row" v-for="(candidate, index) in this.candidates.data">
+              <div class="col-lg-12 col-md-12">
+                <div class="single_jobs white-bg d-flex justify-content-between">
 
-<!--          <div class="job_lists m-0">-->
-<!--            <div class="row">-->
+                  <div class="jobs_left d-flex align-items-center">
+                    <div class="thumb">
+                      <img :src="svgIcon2" :alt="svgIcon2">
+                    </div>
+                    <div class="jobs_conetent">
+                      <a href="#"><h4>{{candidate.user.name}}</h4></a>
+                      <div class="location">
+                        <p>Category: <b>{{candidate.job_category.name}}</b></p>
+                        <p>Level: <b>{{candidate.level.code}}</b></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
 
-<!--              <div class="col-lg-12 col-md-12">-->
-<!--                <div class="single_jobs white-bg d-flex justify-content-between">-->
+          </div>
+          <div class="job_lists m-0" v-else>
+            <div class="row" >
+              <h4>Candidates not found</h4>
+            </div>
+          </div>
+        </div>
 
-<!--                  <div class="jobs_left d-flex align-items-center">-->
-<!--                    <div class="thumb">-->
-<!--                      <img :src="svgIcon2" :alt="svgIcon2">-->
-<!--                    </div>-->
-<!--                    <div class="jobs_conetent">-->
-
-<!--                      <div class="location">-->
-<!--                        Salary from: <b>1500$</b>-->
-<!--                      </div>-->
-
-<!--                      <div class="location">-->
-<!--                        <p>-->
-<!--                          <i class="fa fa-map-marker"></i>-->
-<!--                          Minsk,-->
-<!--                          Belarus-->
-<!--                        </p>-->
-<!--                      </div>-->
-<!--                    </div>-->
-<!--                  </div>-->
-<!--                </div>-->
-<!--              </div>-->
-<!--            </div>-->
-
-<!--            <div class="col-lg-12 col-md-12">-->
-<!--              <div class="single_jobs white-bg d-flex justify-content-between">-->
-
-<!--                <div class="jobs_left d-flex align-items-center">-->
-<!--                  <div class="thumb">-->
-<!--                    <img :src="svgIcon5" :alt="svgIcon5">-->
-<!--                  </div>-->
-<!--                  <div class="jobs_conetent">-->
-
-<!--                    <div class="location">-->
-<!--                      Salary from: <b>2500$</b>-->
-<!--                    </div>-->
-
-<!--                    <div class="location">-->
-<!--                      <p>-->
-<!--                        <i class="fa fa-map-marker"></i>-->
-<!--                        Minsk,-->
-<!--                        Belarus-->
-<!--                      </p>-->
-<!--                    </div>-->
-
-<!--                  </div>-->
-<!--                </div>-->
-<!--              </div>-->
-
-<!--            </div>-->
-<!--          </div>-->
-<!--        </div>-->
-
-<!--        &lt;!&ndash; pagination  &ndash;&gt;-->
-<!--      </div>-->
-<!--    </div>-->
-<!--  </div>-->
-
+        <!-- pagination  -->
+      </div>
+    </div>
+  </div>
   <Footer />
 </template>
+
 
 <script>
 import Header from "./Header";
 import Footer from "./Footer";
+import {GLOBAL_CONSTANTS} from '/src/constants.js';
+import axios from 'axios';
 
 export default {
-  name: "JobBoardVueService",
+  data: function(){
+    return {
+      candidates: [],
+    }
+  },
+  methods:{
+    getCandidates: function () {
+      axios.post(`${GLOBAL_CONSTANTS.APP_JOBSERVICE_URL}/api/candidates/`, {
+        headers: {'Content-Type': 'application/json'}
+      }).then((response) => {
+        console.log(response.data);
+
+        this.candidates = response.data;
+      });
+    }
+  },
   components: {
     Header,
     Footer,
   },
   computed: {
-    // //TODO программное получение ссылок на эти иконки
-    // svgIcon2() {
-    //   return require(`@/assets/img/svg_icon/2.svg`);
-    // },
-    // svgIcon5() {
-    //   return require(`@/assets/img/svg_icon/5.svg`);
-    // },
+    //TODO программное получение ссылок на эти иконки
+    svgIcon2() {
+      return require(`@/assets/img/svg_icon/2.svg`);
+    },
+    svgIcon5() {
+      return require(`@/assets/img/svg_icon/5.svg`);
+    },
+  },
+  mounted(){
+    this.getCandidates();
   }
 }
 </script>
+
