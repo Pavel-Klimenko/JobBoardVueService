3665edaae39d33996d9a7c7196a84303c1243510
Pavel Klimenko <p.klimenko@itachsoft.com>
2024-11-15 17:41:01 +0300

    Made vacancy requesting functionality

--- app/src/components/detail/Vacancy.vue
+++ app/src/components/detail/Vacancy.vue
@@ -39,9 +39,20 @@
               <h4>Job description</h4>
               <p>{{vacancy.description}}</p>
             </div>
-          </div>
+          </div><br />
 
+          <div class="col-md-10" v-if="!didCandidateSentRequest">
+            <div class="submit_btn">
+              <button class="boxed-btn3 w-100 send-data-form" @click.prevent="createVacancyRequest">Request to vacancy</button>
+            </div><br />
+            <p><textarea placeholder="Type your covering letter" cols="150" rows="5" v-model="candidate_covering_letter"></textarea></p><br />
+          </div>
+          <div class="col-md-10" v-else>
+            <h4><b>You have already requested to this vacancy</b></h4>
+          </div>
         </div>
+
+
 
         <div class="col-lg-4">
           <div class="job_sumary">
@@ -88,17 +99,57 @@ export default {
   data: function(){
     return {
       vacancy: false,
+      candidate_covering_letter: '',
+      didCandidateSentRequest: false
     }
   },
   methods:{
     getVacancy: function () {
-      axios.get(`${GLOBAL_CONSTANTS.APP_JOBSERVICE_URL}/api/vacancies/${this.$route.params.id }`, {
+      axios.get(`${GLOBAL_CONSTANTS.APP_JOBSERVICE_URL}/api/vacancies/${this.$route.params.id}`, {
         headers: {'Content-Type': 'application/json'}
       }).then((response) => {
         console.log(response.data);
         this.vacancy = response.data.info.vacancy;
-        disablePreloader();
+        //disablePreloader();
       });
+    },
+    isThereVacancyRequest: function () {
+      axios.get(`${GLOBAL_CONSTANTS.APP_JOBSERVICE_URL}/api/candidates/is-there-vacancy-request?vacancy_id=${this.$route.params.id }`, {
+        headers: {'Content-Type': 'application/json'}
+      }).then((response) => {
+        this.didCandidateSentRequest = response.data.info.is_there_vacancy_request;
+      });
+    },
+
+    //TODO method only for candidate
+    validateForm(candidate_covering_letter) {
+      let result = true;
+      if (!candidate_covering_letter || candidate_covering_letter == '') result = false;
+      return result;
+    },
+
+    //TODO method only for candidate
+    createVacancyRequest: function () {
+      let params =  {
+        vacancy_id: parseInt(this.$route.params.id),
+        candidate_covering_letter: this.candidate_covering_letter,
+      }
+
+      if (!this.validateForm(params.candidate_covering_letter)) {
+        alert('Type the covering letter');
+      } else {
+        console.log('Sending request to vacancy...');
+
+        axios.post(`${GLOBAL_CONSTANTS.APP_JOBSERVICE_URL}/api/candidates/create-vacancy-request`,params, {
+          headers: {'Content-Type': 'application/json'}
+        }).then((response) => {
+          console.log(response);
+          if (response.data.status == 'ok') {
+            //location.reload();
+          }
+          //disablePreloader();
+        });
+      }
     }
   },
   components: {
@@ -107,6 +158,9 @@ export default {
   },
   mounted(){
     this.getVacancy();
+
+    //TODO start only under a candidate
+    this.isThereVacancyRequest();
   }
 }
 </script>
